471dcad366d1f0b6014c12d1e59f51668f574c4d
zsolf <zsolf57@gmail.com>
2024-02-18 00:28:24 +0100

    First fit init

--- DPAA.py
+++ DPAA.py
@@ -17,24 +17,62 @@ lines = file.read().splitlines()
 
 
 #Constants init
-NUMBER_OF_ITEMS = lines[0]
-BIN_CAPACITY = lines[1]
+NUMBER_OF_ITEMS = int(lines[0])
+BIN_CAPACITY = int(lines[1])
 WEIGHTS = []
 
 for i in range(2, len(lines)):
-    WEIGHTS.append(lines[i])
+    WEIGHTS.append(int(lines[i]))
 #------------------------------------#
     
 
 #Algorithm
     
 bins = []
-bin1 = [] #capacity = BIN_CAPACITY
+bin1 = [0] * BIN_CAPACITY #capacity = BIN_CAPACITY
 bins.append(bin1)
 
 #First fit
 
+def load(bin, weight):
+    weightCount = 0
+    for i in range(0,len(bin)):
+          if(bin[i] == 0 and weightCount < weight):
+            bin[i] = weight
+            weightCount+=1
 
+def checkThisBin(bin, currentWeight):
+    available = 0
+    for space in bin:
+        if (space == 0):
+            available+=1
+            if (available == currentWeight):
+                break
+    if (available == currentWeight):
+        load(bin,currentWeight)
+        return 0
+    return 1
+        
+
+
+def checkBins(currentWeight):
+    for bin in bins:
+        if (checkThisBin(bin,currentWeight)==0):
+            break
+        newBin = [0]*1000
+        bins.append(newBin)
+        checkThisBin(newBin,currentWeight)
+        break
+
+for i in range(0, len(WEIGHTS)):
+    
+    currentWeight= WEIGHTS[i]
+    checkBins(currentWeight)
+
+          
+     
+
+print(bins[890])
 
 
 
